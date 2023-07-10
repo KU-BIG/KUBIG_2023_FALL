3f65d020d167e1250921dab2d382e3f027c66253
낭니 <88240443+nang-e@users.noreply.github.com>
2023-07-10 14:45:00 +0900

    week1_김나연

---
+++ 1. 방학분반/파이썬/1주차/week1_ny.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# 1. guess the values and types of each variables
+a = False + 1
+b = "number1" + " b"
+c = 1 + round(3.7)
+
+# 2. 
+arr = [['a','b','c'], [0,1,2], ['jupitar','vscode','colab']]
+
+# 2.1 what's arr[0][2]
+# 2.2 print out 'jupitar' by using index
+# 2.3 delete 'vscode' from this list
+
+# 3. 
+lst = [12,'a','b',13]
+
+# 3.1 append 14 and 15
+# 3.2 reverse the list
+# 3.3 convert list to numpy array 
+
+# 4. [age, name]
+nparr = np.array([[18,'billy'],
+                 [21,'owen'],
+                 [19,'adam'],
+                 [22,'sam'],
+                 [22,'harry']])
+
+# 4.1 return all the ages in the nparr
+# 4.2 return all the names whose age is 22.
+# 4.3 change adam's age to 20
+
+
+
