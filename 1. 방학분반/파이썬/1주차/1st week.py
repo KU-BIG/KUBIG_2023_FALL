4f56f1e987edc290f19cebdf45d0e38d4a4b9c5e
LEJ98 <139002064+LEJ98@users.noreply.github.com>
2023-07-11 18:23:50 +0900

    Add files via upload

---
+++ 1. 방학분반/파이썬/1주차/1st week.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# Problem 1
+
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 1-1 Get the length, max value from the list with some functions
+# 1-2 Delete '6' in the list with method
+# 1-4 Print the cube of numbers by using new list variable 'cube'
+
+
+# Problem 2
+
+person = ['Peter', 'Smith', 'Sam', 'Gwen']
+
+# 2-1 Add 'Charlie' between 'Smith' and 'Sam'
+# 2-2 Sort the list in alphabetical order
+
+
+# Problem 3
+footsize = [['Peter', 275],
+                   ['Smith', 280],
+                   ['Charlie', 260],
+                   ['Sam', 260],
+                   ['Gwen', 240]]
+
+# 3-1 Change list into ndarray 'sizearr'
+# 3-2 Change the Charlie's size into 265
+# 3-3 Make new variable 'size' as list from numbers in 'sizearr'
+# 3-4 Compare the variable index of 'footsize' with 'sizearr'
